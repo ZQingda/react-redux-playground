c7198fc8b57ea50b3671da2ce4a0ef7ca45bccd7
ZQingda <qingda.zou@gmail.com>
2018-06-17 15:47:36 -0400

    Initial commit, react / webpack 4 / babel

---
+++ src/index/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'BAD BOILER 2: REACT-REDUX BOOGALOO';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
